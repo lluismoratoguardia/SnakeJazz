bb7e730bbc05209b6b78b353a4797c30010e1269
lluismoratoguardia <llmoratog@gmail.com>
2024-06-06 15:43:17 +0200

    Create colors and fonts struct, add new font, base utils

---
+++ SnakeJazz/SnakeJazz/Constants/Colors.swift
@@ -0,0 +1,12 @@
+//
+//  ColorConstants.swift
+//  SnakeJazz
+//
+//  Created by Lluís Morató on 6/6/24.
+//
+
+import UIKit
+
+struct Colors {
+    static let backgroundColor = UIColor(red: 217/255, green: 222/255, blue: 154/255, alpha: 1)
+}
---
+++ SnakeJazz/SnakeJazz/Extensions/UIColorExtension.swift
@@ -0,0 +1,8 @@
+//
+//  UIColorExtension.swift
+//  SnakeJazz
+//
+//  Created by Lluís Morató on 6/6/24.
+//
+
+import Foundation
