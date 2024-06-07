c37256cb729a0ad4b7d6c28ff3bc4a78b28f2ad5
lluismoratoguardia <llmoratog@gmail.com>
2024-06-07 12:06:00 +0200

    Create PaginationView

---
+++ SnakeJazz/SnakeJazz/UI Components/Pagination View/PaginationView.swift
@@ -0,0 +1,44 @@
+//
+//  PaginationView.swift
+//  SnakeJazz
+//
+//  Created by Lluís Morató on 7/6/24.
+//
+
+import UIKit
+
+struct PaginationViewModel {
+    let currentPage: Int
+    let totalPages: Int
+    let delegate: PaginationViewProtocol
+}
+
+protocol PaginationViewProtocol {
+    func getPreviousPage()
+    func getNextPage()
+}
+
+class PaginationView: UIView {
+    @IBOutlet private weak var currentPageLabel: UILabel!
+    @IBOutlet private weak var previousPageButton: UIButton!
+    @IBOutlet private weak var nextPageButton: UIButton!
+
+    private var model: PaginationViewModel?
+    
+    func setup(_ model: PaginationViewModel) {
+        self.model = model
+        currentPageLabel.text = "\(model.currentPage) / \(model.totalPages)"
+        previousPageButton.isEnabled = model.currentPage > 1
+        nextPageButton.isEnabled = model.currentPage < model.totalPages
+    }
+
+    // MARK: Actions
+    
+    @IBAction private func previousPageTouchUp() {
+        model?.delegate.getPreviousPage()
+    }
+    
+    @IBAction private func nextPageTouchUp() {
+        model?.delegate.getNextPage()
+    }
+}
